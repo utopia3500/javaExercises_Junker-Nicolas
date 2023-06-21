e5867c529f0429766c5401cda638574f264e2bc7
utopia3500 <nicolas.junker@hotmail.com>
2023-06-21 13:38:36 +0200

    premier commit

---
+++ src/main/java/fr/junker/myApi/MyApiApplication.java
@@ -0,0 +1,13 @@
+package fr.junker.myApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/junker/myApi/MyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.junker.myApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
