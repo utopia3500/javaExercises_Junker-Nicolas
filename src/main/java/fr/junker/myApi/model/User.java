3a3608419faaff4fa1d28b7aa2215995b7f7970a
utopia3500 <nicolas.junker@hotmail.com>
2023-06-23 14:53:59 +0200

    dernier commit

--- src/main/java/fr/junker/myApi/model/User.java
+++ src/main/java/fr/junker/myApi/model/User.java
@@ -1,11 +1,7 @@
 package fr.junker.myApi.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
+
+
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,17 +9,13 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-@Entity
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-@NamedQueries({ @NamedQuery(name = "User.findAll", query = "select u from User u ")
-})
 public class User {
-    @Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private Integer age;
